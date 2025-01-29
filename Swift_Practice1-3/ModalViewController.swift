2497bee45626ce0f54b9cac1636eea3c93be42ee
42kharuya <haruya.0411.k@gmail.com>
2025-01-29 14:53:48 +0900

    completed app, moving the other page.

---
+++ Swift_Practice1-3/ModalViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ModalViewController.swift
+//  Swift_Practice1-3
+//
+//  Created by 川﨑遥也 on 2025/01/29.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func BackButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
